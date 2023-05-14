4fd6c2fa1b44bf0dca9314b4a2a1e0d5d0084e4e
YassineOurara <yassineourara@gmail.com>
2023-05-14 23:55:25 +0100

    Bank account

--- bank-account-service/src/main/java/org/sid/bankaccountservice/enums/AccountType.java
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/enums/AccountType.java
@@ -1,7 +1,5 @@
 package org.sid.bankaccountservice.enums;
 
-public class AccountType {
-    public static final Object CURRENT_ACCOUNT = ;
-    public static final Object SAVING_ACCOUNT = ;
-    CURRENT_ACCOUNT , SAVING_ACCOUNT;
+public enum AccountType {
+    CURRENT_ACCOUNT,SAVING_ACCOUNT
 }
