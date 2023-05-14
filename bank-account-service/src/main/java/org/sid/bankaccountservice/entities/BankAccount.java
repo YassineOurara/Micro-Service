6ef45788c03356e52302676fca2b8a6bfdc6ea39
YassineOurara <yassineourara@gmail.com>
2023-05-14 23:45:10 +0100

    Bank account

---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/entities/BankAccount.java
@@ -0,0 +1,24 @@
+package org.sid.bankaccountservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.bankaccountservice.enums.AccountType;
+
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAT;
+    private double balance;
+    private String currency;
+    private AccountType type;
+
+}
---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/enums/AccountType.java
@@ -0,0 +1,7 @@
+package org.sid.bankaccountservice.enums;
+
+public class AccountType {
+    public static final Object CURRENT_ACCOUNT = ;
+    public static final Object SAVING_ACCOUNT = ;
+    CURRENT_ACCOUNT , SAVING_ACCOUNT;
+}
